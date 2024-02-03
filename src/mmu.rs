b929a6b8bbd71fb1c7858a7a0ba8512bbd259c0f
S Takumi <takumi.shiratori@gmail.com>
2024-02-03 15:14:28 +0900

    impl MBC1

--- src/mmu.rs
+++ src/mmu.rs
@@ -4,24 +4,39 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 
+const ROM_BANK_BIT_MAX: usize = 7;
+const RAM_BANK_BIT_MAX: usize = 3;
+const ROM_BANK_MAX: usize = 1 << ROM_BANK_BIT_MAX;
+const RAM_BANK_MAX: usize = 1 << RAM_BANK_BIT_MAX;
+
 pub struct MMU {
     inbios: bool,
-    bios: [u8; 0x0100],  // GameBoy BIOS code 256 byte
-    rom: [u8; 0x8000],   // Cartridge ROM 32k byte
-    vram: [u8; 0x2000],  // Graphics RAM 8k byte
-    eram: [u8; 0x2000],  // Cargridge (External) RAM 8k byte
-    wram: [u8; 0x2000],  // Working RAM 8k byte
-    ioreg: [u8; 0x0080], // I/O Registers
-    zram: [u8; 0x0080],  // Zero-page Ram 128 byte
+    ram_enable: bool,
+    rom_bank_bit: usize,
+    ram_bank_bit: Option<usize>,
+    rom_bank: usize,
+    ram_bank: usize,
+    bios: [u8; 0x0100],                // GameBoy BIOS code 256 byte
+    rom: [u8; 0x4000 * ROM_BANK_MAX],  // Cartridge ROM 32k byte
+    vram: [u8; 0x2000 * RAM_BANK_MAX], // Graphics RAM 8k byte
+    eram: [u8; 0x2000],                // Cargridge (External) RAM 8k byte
+    wram: [u8; 0x2000],                // Working RAM 8k byte
+    ioreg: [u8; 0x0080],               // I/O Registers
+    zram: [u8; 0x0080],                // Zero-page Ram 128 byte
 }
 
 impl MMU {
     pub fn new() -> MMU {
         MMU {
             inbios: true,
+            ram_enable: false,
+            rom_bank_bit: ROM_BANK_BIT_MAX,
+            ram_bank_bit: Some(RAM_BANK_BIT_MAX),
+            rom_bank: 1,
+            ram_bank: 0,
             bios: [0; 0x0100],
-            rom: [0; 0x8000],
-            vram: [0; 0x2000],
+            rom: [0; 0x4000 * ROM_BANK_MAX],
+            vram: [0; 0x2000 * RAM_BANK_MAX],
             eram: [0; 0x2000],
             wram: [0; 0x2000],
             ioreg: [0; 0x0080],
@@ -32,9 +47,6 @@ impl MMU {
     pub fn load(&mut self, filename: &str) -> Result<(), Box<dyn Error>> {
         let buf = BufReader::new(File::open(filename)?);
         for (i, byte_or_error) in buf.bytes().enumerate() {
-            if i >= 0x8000 {
-                break;
-            }
             let byte = byte_or_error.unwrap();
             self.rom[i] = byte;
         }
@@ -103,9 +115,13 @@ impl MemoryIF for MMU {
             //}
             //}
             // ROM1 (unbanked) 16k
-            0x0000..=0x7fff => {
+            0x0000..=0x3fff => {
                 let index = addr as usize;
                 self.rom[index]
+            }
+            0x4000..=0x7fff => {
+                let index = (addr - 0x4000) as usize;
+                self.rom[self.rom_bank * 0x4000 + index]
             }
             // Graphics: VRAM 8k
             0x8000..=0x9fff => {
@@ -114,8 +130,12 @@ impl MemoryIF for MMU {
             }
             // External RAM 8k
             0xa000..=0xbfff => {
-                let index = (addr - 0xa000) as usize;
-                self.eram[index]
+                if self.ram_enable {
+                    let index = (addr - 0xa000) as usize;
+                    self.eram[self.ram_bank * 0x2000 + index]
+                } else {
+                    0xff
+                }
             }
             // Working RAM 8k
             0xc000..=0xdfff => {
@@ -146,8 +166,37 @@ impl MemoryIF for MMU {
     fn write_byte(&mut self, addr: u16, val: u8) {
         match addr {
             // ROM1 (unbanked) 16k
-            0x0000..=0x7fff => {
-                panic!("write_byte:rom 0x{:x}", addr);
+            // 0x0000..=0x7fff => {
+            //     panic!("write_byte:rom 0x{:x}, val: {}", addr, val);
+            // }
+            // RAM Enable
+            0x0000..=0x1fff => {
+                if val & 0x0f == 0x0a {
+                    if self.ram_bank_bit == None {
+                        panic!("write_byte: ram_bank_num is 0");
+                    }
+                    self.ram_enable = true;
+                } else {
+                    self.ram_enable = false;
+                }
+            }
+            // ROM Bank Number
+            0x2000..=0x3fff => {
+                let val = val & 0x1f;
+                let val = if val == 0 { 1 } else { val };
+                let val = val & ((1 << self.rom_bank_bit) - 1);
+                self.rom_bank = (self.rom_bank & 0xe0) + val as usize;
+            }
+            // RAM Bank Number
+            0x4000..=0x5fff => {
+                let ram = val & 0x03;
+                let rom = (val & 0xc0) >> 6;
+                self.ram_bank = ram as usize;
+                self.rom_bank = (self.rom_bank & 0x1f) + (rom << 5) as usize;
+            }
+            // Banking Mode Select
+            0x6000..=0x7fff => {
+                panic!("write_byte:rom 0x{:x}, val: {}", addr, val);
             }
             // Graphics: VRAM 8k
             0x8000..=0x9fff => {
